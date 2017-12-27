9bf44cfc789aaca77bfd0ed8f71990ba92aa9101
truong quoc khanh <cuk2dn96@gmail.com>
2017-12-28 00:56:05 +0700

    Add project files.

---
+++ MVC/Models/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class DataBaseContext : DbContext
+    {
+       public DbSet<Student> Students { get; set; }
+        public DbSet<Lop> Lops { get; set; }
+    }
+}
---
+++ MVC/Models/Lop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Lop
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
